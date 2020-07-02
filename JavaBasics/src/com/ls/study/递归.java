17da25aec846b84216bce86223e579057d9f62b9
bigWhite <bigWhite@qq.com>
2020-07-02 21:52:10 +0800

    修改方法“递归”

--- JavaBasics/src/com/ls/study/递归.java
+++ JavaBasics/src/com/ls/study/递归.java
@@ -5,6 +5,7 @@ public class 递归 {
     public static void main(String[] args) {
 
         System.out.println(f(5));
+        System.out.println("hello!world");
 
     }
 
@@ -18,5 +19,6 @@ public class 递归 {
     public void test(){
         int i = 0;
         i+=10;
+        System.out.println(i);
     }
 }
